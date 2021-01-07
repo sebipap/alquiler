c74dd942267be5ef0e71aa3ccf3dc59289581c0b
Sebastian Papanicolau <sebipaps@gmail.com>
2021-01-07 00:43:09 -0300

    beta 1.0.2

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config()
 const config = {
-    serverURL: 'https://floating-falls-17629.herokuapp.com/',
+    serverURL: 'https://auto-trader-arg.herokuapp.com/',
     //serverURL: 'https://localhost:5000',
     //serverURL: '',
 }
